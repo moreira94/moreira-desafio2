ae2998ed7663505e2a13486668ed046b7dd5e82a
Julian Moreira <moreirajulian.gustavo@gmail.com>
2024-04-24 10:09:49 -0300

    me mudo de compu

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 
 const __filename =fileURLToPath(import.meta.url)
 
-export const __dirname = dirname(__filename);;
+export const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
